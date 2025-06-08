ad264d980efd5f843ca79e24e8ff9a50b8b5ea83
Valentin Favard <lefav@MacBook-Pro-de-Valentin.local>
2025-06-08 15:18:06 +0200

    parsing

--- sources/init.c
+++ sources/init.c
@@ -6,15 +6,17 @@
 /*   By: vafavard <vafavard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/08 09:01:42 by vafavard          #+#    #+#             */
-/*   Updated: 2025/06/08 14:13:59 by vafavard         ###   ########.fr       */
+/*   Updated: 2025/06/08 15:16:56 by vafavard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include <limits.h>
 
 t_node	*ft_lstnew(void *content);
 int	ft_isdigit(int c);
 void	ft_lstadd_back(t_node **lst, t_node *new);
+long	ft_atol(const char *str);
 
 void	init_stack_a(t_node **a, t_node **b, char **values, int string)
 {
@@ -32,21 +34,20 @@ void	init_stack_a(t_node **a, t_node **b, char **values, int string)
 		i = 0;
 		while (values[i])
 		{
-			//verifier contenu
-			//atoi ou atol ?
-			//verifier doublons
-			//fill stack a
-			ft_lstnew();
+			if (!is_valid_number(values[i]))
+				ft_error(a, b, values, string);
+			if (!check_duplicates(*a, (int)nbr))
+				ft_error(a, b, values, string);
+			nbr = ft_atol(values[i]);
+			if (nbr > INT_MAX || nbr < INT_MIN)
+				ft_error(a, b, values, string);
+			new = ft_lstnew((int)nbr);
 			if (!new)
 				ft_error(a, b, values, string);
-			
-				//tout free
 			lst_add_back(a, new);
 			i++;
 		}
-		
 	}
-
 }
 
 int	is_valid_number(char *str)
@@ -102,4 +103,40 @@ int	ft_isdigit(int c)
 	if (c >= 48 && c <= 57)
 		return (1);
 	return (0);
+}
+
+long	ft_atol(const char *str)
+{
+	int		i;
+	long	nb;
+	int		sign;
+
+	i = 0;
+	nb = 0;
+	sign = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || (str[i] == 32))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nb = (nb * 10) + (str[i] - '0');
+		i++;
+	}
+	return (nb * sign);
+}
+
+int	check_duplicates(t_node *stack, int value)
+{
+	while(stack)
+	{
+		if (stack->next == value)
+			return (0);
+		stack = stack->next;
+	}
+	return (1);
 }
