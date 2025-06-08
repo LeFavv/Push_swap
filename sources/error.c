95b11d39ac35348076925d352c65e5bdb5bbd5c1
Valentin Favard <lefav@MacBook-Pro-de-Valentin.local>
2025-06-08 15:51:23 +0200

    free_all

--- sources/error.c
+++ sources/error.c
@@ -6,7 +6,7 @@
 /*   By: vafavard <vafavard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/08 15:19:46 by vafavard          #+#    #+#             */
-/*   Updated: 2025/06/08 15:27:24 by vafavard         ###   ########.fr       */
+/*   Updated: 2025/06/08 15:50:43 by vafavard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,31 @@ int	ft_error(t_node **a, t_node **b, char **values, int string)
 	write (1, "Error\n", 6);
 	free_all(a, b, values, string);
 	exit(1);
+}
+
+void	free_stack(t_node **a)
+{
+	t_node *node;
+	
+	while (*a)
+	{
+		node = (*a)->next;
+		free(*a);
+		*a = node;
+	}
+	*a = NULL;
+}
+
+void	free_split(char **values)
+{
+	int i;
+	
+	i = 0;
+	while (values[i])
+	{
+		free(values[i]);
+		i++;
+	}
 }
 
 void	ft_free_all(t_node **a, t_node **b, char **values, int string)
