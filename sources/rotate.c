56651ac5da3ba034a7a663fbecd7feda850b937b
Valentin Favard <lefav@MacBook-Pro-de-Valentin.local>
2025-06-09 15:11:20 +0200

    rotate

--- sources/rotate.c
+++ sources/rotate.c
@@ -6,10 +6,43 @@
 /*   By: vafavard <vafavard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/08 17:45:23 by vafavard          #+#    #+#             */
-/*   Updated: 2025/06/09 14:32:21 by vafavard         ###   ########.fr       */
+/*   Updated: 2025/06/09 15:10:31 by vafavard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	ra(t_node **a);
+void	rb(t_node **b);
+void	rr(t_node **a, t_node **b);
+
+void    rotate(t_node **stack)
+{
+   t_node   *first;
+   t_node   *last;
+
+   first = *stack;
+   *stack = first->next;
+   first->next = NULL;
+   last = ft_lstlast(*stack);
+   last->next = first;
+}
+void    ra(t_node **a)
+{
+    ft_printf("ra\n");
+    rotate(a);
+}
+
+void	rb(t_node **b)
+{
+    ft_printf("rb\n");
+    rotate(b);
+}
+
+void rr(t_node **a, t_node **b)
+{
+    ft_printf("rr\n");
+    rotate(a);
+    rotate(b);
+}
 
